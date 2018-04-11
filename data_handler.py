375df5e9197019edc38c17e73986fbf762ff1c35
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2018-04-11 18:08:21 -0400

    Add files via upload

--- data_handler.py
+++ data_handler.py
@@ -2,18 +2,18 @@ import codecs
 
 def get_data():
 	tweets = []
-	with codecs.open('./'+'tweets.txt', 'r', encoding='utf-8') as f:
+	with codecs.open('./'+'tokenized_tweets.txt', 'r', encoding='utf-8') as f:
 		data = f.readlines()
 	for line in data:
+		line = line
 		a = line.split()
-		b = a[-2]
 		c = a[-1]
-		a.pop()
 		a.pop()
 		a = ' '.join(a)
 		tweets.append({
-                'text': a.lower(),
+                'text': a,
                 'label': c,
-                'user': b
                 })
 	return tweets
+
+
